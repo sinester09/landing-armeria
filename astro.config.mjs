352bf8ae616ff84b0b839a24dc3927b99c5abf25
sinester09 <60792457+sinester09@users.noreply.github.com>
2025-03-27 17:44:22 -0500

    Update astro.config.mjs
    correcion deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sinester09.github.io',
+  base: '/landing-armeria', // Importante: debe coincidir con el nombre del repositorio
+});
