f21c74bf6465f1a0eb09d87529929fa75f79b454
Héctor <hgl502@hotmail.com>
2025-03-28 12:29:46 -0500

    correccion astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://sinester09.github.io',
   base: '/landing-armeria',
-   build: {
-    assets: true, // Asegura que los assets se manejen correctamente
+  build: {
+    assets: "assets", // Asegura que los assets se manejen correctamente
   },
   vite: {
     build: {
-      assetsInlineLimit: 0 // Previene la incrustación de imágenes como base64
+      assetsInlineLimit: 0, // Previene la incrustación de imágenes como base64
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/[name].[hash][extname]" // ✅ Correcto
+        }
+      }
     }
   }
 });
